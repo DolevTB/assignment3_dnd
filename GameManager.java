7cf0e9b407ba5a872bc806aba816be0eb2153cad
Kimbo <kimbog@post.bgu.ac.il>
2026-06-28 15:37:36 +0300

    added new class - GameManager

---
+++ GameManager.java
@@ -0,0 +1,15 @@
+package assignment3_dnd;
+
+public class GameManager {
+
+    public GameManager() {
+        // Initialize game state, create map, place units, etc.
+    }
+
+    public void StartGame() {
+        // Main game loop
+        while (true) {
+            // Handle player input, update game state, render map, etc.
+        }
+    }
+}
