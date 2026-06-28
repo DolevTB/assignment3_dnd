ddd0f516d3ffabf49cdc0264bd16366f2f654943
dolevTB <dolevgeva@gmail.com>
2026-06-28 14:06:58 +0300

    enemy classes were added

--- Monster.java
+++ Monster.java
@@ -1,16 +1,42 @@
 package assignment3_dnd;
 
-public class Monster extends Enemy{
+import java.util.Random;
+
+public class Monster extends Enemy {
     private int vision_range;
 
-    public Monster(String name, int hp_pool, int atk_pts, int def_pts, int exp_val, int vision_range) {
-        super(name, hp_pool, atk_pts, def_pts, exp_val);
+    public Monster(char tile, String name, int hp_pool, int atk_pts, int def_pts, int exp_val, int vision_range) {
+        super(tile, name, hp_pool, atk_pts, def_pts, exp_val);
         this.vision_range = vision_range;
     }
 
-    public void Move(Position player_pos) {
-
+    public Position Move(Position myPos, Position playerPos) {
+        double range = myPos.Range(playerPos);
+        if (range < vision_range) {
+            int dx = myPos.GetX() - playerPos.GetX();
+            int dy = myPos.GetY() - playerPos.GetY();
+            if (Math.abs(dx) > Math.abs(dy)) {
+                if (dx > 0) return new Position(myPos.GetX() - 1, myPos.GetY());
+                else return new Position(myPos.GetX() + 1, myPos.GetY());
+            } else {
+                if (dy > 0) return new Position(myPos.GetX(), myPos.GetY() - 1);
+                else return new Position(myPos.GetX(), myPos.GetY() + 1);
+            }
+        } 
+        else {
+            Random rand = new Random();
+            int move = rand.nextInt(5);
+            return switch (move) {
+                case 0 -> new Position(myPos.GetX() - 1, myPos.GetY());
+                case 1 -> new Position(myPos.GetX() + 1, myPos.GetY());
+                case 2 -> new Position(myPos.GetX(), myPos.GetY() - 1);
+                case 3 -> new Position(myPos.GetX(), myPos.GetY() + 1);
+                default -> myPos;
+            };
+        }
     }
 
-    public int GetVisionRange() { return vision_range; }
+    public int GetVisionRange() { 
+        return vision_range; 
+    }
 }
---
+++ Trap.java
@@ -0,0 +1,33 @@
+package assignment3_dnd;
+
+public class Trap extends Enemy {
+    private int visibility_time;
+    private int invisibility_time;
+    private int ticks_count;
+    private boolean visible;
+
+    public Trap(char tile, String name, int hp_pool, int atk_pts, int def_pts, int exp_val, int visibility_time, int invisibility_time) {
+        super(tile, name, hp_pool, atk_pts, def_pts, exp_val);
+        this.visibility_time = visibility_time;
+        this.invisibility_time = invisibility_time;
+        this.ticks_count = 0;
+        this.visible = true;
+    }
+
+    public boolean Tick(Position myPosition, Position targetPosition) {
+        visible = ticks_count < visibility_time;
+        
+        if (ticks_count == (visibility_time + invisibility_time)) {
+            ticks_count = 0;
+        }
+        else {
+            ticks_count++;
+        }
+        return myPosition.Range(targetPosition) < 2;
+    }
+
+    @Override
+    public String toString() {
+        return visible ? String.valueOf(super.tile) : ".";
+    }
+}
